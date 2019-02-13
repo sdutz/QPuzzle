4693136c93d9f6c739ab8e5e18185fd442547f7a
sdutz <zambellilorenzo@gmail.com>
2019-02-13 22:07:10 +0100

    Fixed settings in windows

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    PuzzleWnd w;
+    QApplication a(argc, argv) ;
+
+    QCoreApplication::setOrganizationName( "Sdutz") ;
+    QCoreApplication::setOrganizationDomain( "https://github.com/sdutz/") ;
+    QCoreApplication::setApplicationName( "QPuzzle") ;
+
+    PuzzleWnd w ;
     w.show();
 
     return a.exec();
